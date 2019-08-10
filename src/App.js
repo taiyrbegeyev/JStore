b2df9802b914d6b87e5c8f2f82c61d6aa67809b3
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-08-10 19:43:25 +0200

    :rotating_light: fix linting errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <React.Fragment>
+        Hello World
+      </React.Fragment>
+    )
+  }
 }
 
-export default App;
+export default App
