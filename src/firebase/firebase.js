0870f0eed8b9f05d01c816691f013a1f86a6f70f
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-08-11 00:11:54 +0200

    :wrench: export firebase functions

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,3 +14,6 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig)
 export default firebase
+export const storage = firebase.storage();
+export const auth = firebase.auth();
+export const db = firebase.database();
