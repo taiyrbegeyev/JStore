b47bcedec78a30254f9eece6dce2f1e291459949
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-08-11 21:55:44 +0200

    :sparkles: create new page

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Header from 'components/Header/Header'
+
+class Landing extends Component {
+  render() {
+    return (
+      <Header />
+    )
+  }
+}
+
+export default Landing
