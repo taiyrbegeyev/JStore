72d1c74a337c5310883dbb041bd9146dc197e5cc
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-08-13 23:49:59 +0200

    :sparkles: create button and input components

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Button = styled.input`
+  cursor: pointer;
+  -webkit-appearance: button;
+  background-color: #004180;
+  color: #ffffff;
+  border-radius: 7px;
+  font-style: normal;
+  font-weight: 400;
+  width: 100%;
+  padding: 0.5rem 0;
+  line-height: 2.75;
+  font-size: 1.6rem;
+`
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Input = styled.input`
+  border: 1px solid #e6e6e6;
+  border-radius: 0.7rem;
+  outline-width: 0.1rem;
+  width: 100%;
+  color: #333333;
+  padding: 12px 16px;
+  line-height: 1.2;
+  font-size: 1.6rem;
+`
