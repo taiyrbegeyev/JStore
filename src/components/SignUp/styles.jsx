dda9bc3519b479991d5c0721559bc8d4e791aba2
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-08-14 22:48:52 +0200

    :iphone: make sign up responsive

--- src/components/SignUp/styles.jsx
+++ src/components/SignUp/styles.jsx
@@ -1,21 +1,29 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import { Card } from '@material-ui/core'
+// import { Card } from '@material-ui/core'
 
-export const SignUpContainer = styled(Card)`
-  &&& {
-    max-width: 51.2rem;
+export const SignUpContainer = styled.div`
+  width: 100%;
+  max-width: 51.2rem;
+  @media (min-width: 768px) {
     padding: 6.2rem 10rem 4.8rem;
+    box-shadow: 0px 4px 24px rgba(0, 0, 0, 0.1);
   }
 `
 
 export const Heading = styled.div`
   text-align: center;
-  margin-bottom: 4.5rem;
+  margin-bottom: 2rem;
+  @media (min-width: 768px) {
+    margin-bottom: 4.5rem;
+  }
 `
 
 export const LogoImage = styled.img`
-  width: 18rem;
+  width: 16rem;
+  @media (min-width: 768px) {
+    width: 18rem;
+  }
 `
 
 export const SignUpForm = styled.form`
@@ -24,11 +32,13 @@ export const SignUpForm = styled.form`
 `
 
 export const InputContainer = styled.div`
+  margin: auto;
   width: 30rem;
   margin-bottom: 1rem;
 `
 
 export const ButtonContainer = styled.div`
+  margin: auto;
   width: 30rem;
   margin-top: 0.7rem;
   margin-bottom: 1.4rem;
