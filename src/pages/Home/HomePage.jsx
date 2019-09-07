6f8e71cf6572341a571d7658cf32d72848f4da94
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-09-07 09:50:04 +0200

    :sparkles: create new page

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HomePage extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        
+      </React.Fragment>
+    )
+  }
+}
+
+export default HomePage
