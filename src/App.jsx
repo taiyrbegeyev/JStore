fa67a27d42baa76e72d2c0bf6560f68e85865815
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-09-20 19:25:28 +0200

    :fire: don't use privgate routes anymore

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,29 @@
 import React, { Component } from 'react'
 import { Switch, Route, BrowserRouter as Router, Redirect } from 'react-router-dom'
 import { routes } from 'routing'
-import { auth } from 'firebase.js'
 
-const PrivateRoute = ({ auth: auth, component: Component, ...rest }) => (
-  <Route
+const PrivateRoute = ({ auth, component: Component, ...rest }) => {
+  console.log(auth)
+  
+  return <Route
     {...rest}
     render={props => (
-      auth
+      auth === true
       ? <Component {...props} />
       : <Redirect to='get-started' />
     )
     }
   />
-)
+}
 
 class App extends Component {
-  state = {
-    isAuth: false
-  }
-  
-  componentDidMount = () => {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({
-          isAuth: true
-        })
-      }
-    })
-  }
-  
   render () {
     const elements = routes.map((item, index) => {
-      const { path, exact, isPrivate, component } = item
+      const { path, exact, component } = item
       const routeProps = { path, exact }
-      if (isPrivate) {
-        return (
-          <PrivateRoute auth={auth} key={index} {...routeProps} component={component} />
-        )
-      } else {
-        return (
-          <Route key={index} {...routeProps} component={component} />
-        )
-      }
+      return (
+        <Route key={index} {...routeProps} component={component} />
+      )
     })
         
     return (
