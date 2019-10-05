19dd003eafacddcb6b709cd7539358c3147cdf35
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-10-05 15:00:37 +0200

    :sparkles: introduce price input

---
+++ src/components/PriceInput/PriceInput.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import FormControl from '@material-ui/core/FormControl'
+import { withStyles } from '@material-ui/core'
+
+const useStyles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    [theme.breakpoints.up('sm')]: {
+      width: 300
+    },
+  },
+  margin: {
+    margin: theme.spacing(2),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+})
+
+class PriceInput extends Component {
+  state = {
+    amount: null
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      amount: e.target.value
+    }, () => {
+      this.props.parentCallBack (this.props.data_name, this.state.amount)
+      window.localStorage.setItem(this.props.data_name, this.state.amount)
+    })
+  }
+  
+  render() {
+    const { classes, data_name, error } = this.props
+    const { amount } = this.state
+   
+    return (
+      <div className={classes.root}>
+        <FormControl fullWidth className={classes.margin}>
+          <InputLabel htmlFor="adornment-amount">Amount</InputLabel>
+          <Input
+            id="adornment-amount"
+            fullWidth
+            defaultValue={window.localStorage.getItem(data_name)}
+            value={amount}
+            onChange={this.handleChange}
+            startAdornment={<InputAdornment position="start">€</InputAdornment>}
+            error={error}
+          />
+        </FormControl>
+      </div>
+    )
+
+  }
+}
+
+export default withStyles(useStyles)(PriceInput)
