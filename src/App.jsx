a61285245817b7a403fb28fc831aa0b7b2b63ee5
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-10-23 14:36:32 +0200

    :bug: fix auth from different devices

--- src/App.jsx
+++ src/App.jsx
@@ -73,45 +73,42 @@ class App extends Component {
         if (!email && !this.state.user) {
           email = window.prompt('Please provide your email for confirmation')
         }
-        else {
-          auth.signInWithEmailLink(email, window.location.href)
-          .then((result) => {
-            console.log('signInWithEmailLink')
-            window.localStorage.removeItem('emailForSignIn')
-
-            if (result.user) {
-              console.log('signInWithEmailLink setting to true')
-              userExists(email, (res) => {
-                this.setState({
-                  isNewUser: !res 
-                })
-              })
+        auth.signInWithEmailLink(email, window.location.href)
+        .then((result) => {
+          console.log('signInWithEmailLink')
+          window.localStorage.removeItem('emailForSignIn')
+          if (result.user) {
+            console.log('signInWithEmailLink setting to true')
+            userExists(email, (res) => {
               this.setState({
-                isAuth: true,
-                user: result.user,
-                isLoading: false
+                isNewUser: !res 
               })
-            }
-            else {
-              console.log('signInWithEmailLink setting to false')
-              this.setState({
-                isAuth: false,
-                user: null,
-                isLoading: false
-              })
-            }
-          })
-          .catch((err) => {
-            console.log(err)
-            alert('Error: ' + err.message)
-            auth.signOut()
+            })
+            this.setState({
+              isAuth: true,
+              user: result.user,
+              isLoading: false
+            })
+          }
+          else {
+            console.log('signInWithEmailLink setting to false')
             this.setState({
               isAuth: false,
               user: null,
               isLoading: false
             })
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+          alert('Error: ' + err.message)
+          auth.signOut()
+          this.setState({
+            isAuth: false,
+            user: null,
+            isLoading: false
           })
-        }
+        })
       }
       else {
         console.log('onAuthStateChanged setting to false ')
